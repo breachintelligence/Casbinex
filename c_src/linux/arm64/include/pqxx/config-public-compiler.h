b685bb2a2f7cf6db9a6cc2b002820eba748adcd8
William Michaels <bill@polarity.io>
2022-07-05 10:26:02 -0500

    fix: proper includes for libpqxx

--- c_src/linux/arm64/include/pqxx/config-public-compiler.h
+++ c_src/linux/arm64/include/pqxx/config-public-compiler.h
@@ -1,121 +1,81 @@
 /* include/pqxx/config.h.in.  Generated from configure.ac by autoheader.  */
-
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#undef HAVE_DLFCN_H
-
+/* #undef HAVE_DLFCN_H */
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
-
+/* #undef HAVE_INTTYPES_H */
 /* Define to 1 if you have the `pq' library (-lpq). */
-#undef HAVE_LIBPQ
-
+/* #undef HAVE_LIBPQ */
 /* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
-
+/* #undef HAVE_MEMORY_H */
 /* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
-
+/* #undef HAVE_STDINT_H */
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
-
+/* #undef HAVE_STDLIB_H */
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
-
+/* #undef HAVE_STRINGS_H */
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
-
+/* #undef HAVE_STRING_H */
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
-
+/* #undef HAVE_SYS_STAT_H */
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
-
+/* #undef HAVE_SYS_TYPES_H */
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
-
+/* #undef HAVE_UNISTD_H */
 /* Define to the sub-directory where libtool stores uninstalled libraries. */
-#undef LT_OBJDIR
-
+/* #undef LT_OBJDIR */
 /* Name of package */
-#undef PACKAGE
-
+/* #undef PACKAGE */
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
-
+/* #undef PACKAGE_BUGREPORT */
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
-
+/* #undef PACKAGE_NAME */
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
-
+/* #undef PACKAGE_STRING */
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
-
+/* #undef PACKAGE_TARNAME */
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
-
+/* #undef PACKAGE_URL */
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
-
+/* #undef PACKAGE_VERSION */
 /* Define if <charconv> supports floating-point conversion. */
-#undef PQXX_HAVE_CHARCONV_FLOAT
-
+/* #undef PQXX_HAVE_CHARCONV_FLOAT */
 /* Define if <charconv> supports integer conversion. */
-#undef PQXX_HAVE_CHARCONV_INT
-
+#define PQXX_HAVE_CHARCONV_INT
 /* Define if compiler has C++20 std::cmp_greater etc. */
-#undef PQXX_HAVE_CMP
-
+/* #undef PQXX_HAVE_CMP */
 /* Define if compiler supports Concepts and <ranges> header. */
-#undef PQXX_HAVE_CONCEPTS
-
+/* #undef PQXX_HAVE_CONCEPTS */
 /* Define if compiler supports __cxa_demangle */
-#undef PQXX_HAVE_CXA_DEMANGLE
-
+#define PQXX_HAVE_CXA_DEMANGLE
 /* Define if g++ supports pure attribute */
-#undef PQXX_HAVE_GCC_PURE
-
+#define PQXX_HAVE_GCC_PURE
 /* Define if g++ supports visibility attribute. */
-#undef PQXX_HAVE_GCC_VISIBILITY
-
+#define PQXX_HAVE_GCC_VISIBILITY
 /* Define if likely & unlikely work. */
-#undef PQXX_HAVE_LIKELY
-
+/* #undef PQXX_HAVE_LIKELY */
 /* Define if operator[] can take multiple arguments. */
-#undef PQXX_HAVE_MULTIDIMENSIONAL_SUBSCRIPT
-
+/* #undef PQXX_HAVE_MULTIDIMENSIONAL_SUBSCRIPT */
 /* Define if compiler has usable std::filesystem::path. */
-#undef PQXX_HAVE_PATH
-
+#define PQXX_HAVE_PATH
 /* Define if poll() is available. */
-#undef PQXX_HAVE_POLL
-
+#define PQXX_HAVE_POLL
 /* Define if libpq has PQencryptPasswordConn (since pg 10). */
-#undef PQXX_HAVE_PQENCRYPTPASSWORDCONN
-
+#define PQXX_HAVE_PQENCRYPTPASSWORDCONN
 /* Define if libpq has pipeline mode (since pg 14). */
-#undef PQXX_HAVE_PQ_PIPELINE
-
+#define PQXX_HAVE_PQ_PIPELINE
 /* Define if std::this_thread::sleep_for works. */
-#undef PQXX_HAVE_SLEEP_FOR
-
+#define PQXX_HAVE_SLEEP_FOR
 /* Define if compiler has std::span. */
-#undef PQXX_HAVE_SPAN
-
+/* #undef PQXX_HAVE_SPAN */
 /* Define if strerror_r() is available. */
-#undef PQXX_HAVE_STRERROR_R
-
+#define PQXX_HAVE_STRERROR_R
 /* Define if strerror_s() is available. */
-#undef PQXX_HAVE_STRERROR_S
-
+/* #undef PQXX_HAVE_STRERROR_S */
 /* Define if thread_local is fully supported. */
-#undef PQXX_HAVE_THREAD_LOCAL
-
+#define PQXX_HAVE_THREAD_LOCAL
 /* Define if std::chrono has year_month_day etc. */
-#undef PQXX_HAVE_YEAR_MONTH_DAY
-
+/* #undef PQXX_HAVE_YEAR_MONTH_DAY */
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
-
+/* #undef STDC_HEADERS */
 /* Version number of package */
-#undef VERSION
+/* #undef VERSION */
